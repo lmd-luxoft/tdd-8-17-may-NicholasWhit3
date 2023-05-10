5df2e648590a8df84479cf0fbeaf9062edb86487
Mykola Timchenko <64132625+NicholasWhit3@users.noreply.github.com>
2023-05-10 11:46:03 +0300

    Uploading TDDKata folder manually

---
+++ TDDKata/UnitTest_Calc.cs
@@ -0,0 +1,37 @@
+using TDDKataCalc;
+
+namespace TDDKata
+{
+    public class UnitTest_Calc
+    {
+        private readonly Calc calculator = new();
+
+        [Fact]
+        public void Add_EmptyString_ReturnsZero()
+        {
+            int sum = calculator.Add("");
+            Assert.Equal(0, sum);
+        }
+
+        [Fact]
+        public void Add_SingleNumberString_ReturnsNumber()
+        {
+            int sum = calculator.Add("1");
+            Assert.Equal(1, sum);
+        }
+
+        [Fact]
+        public void Add_TwoNumbersString_ReturnsSum()
+        {
+            int sum = calculator.Add("1,2");
+            Assert.Equal(3, sum);
+        }
+
+        [Fact]
+        public void Add_MultipleNumbersString_ReturnsSum()
+        {
+            int sum = calculator.Add("1,2,3,4,5");
+            Assert.Equal(15, sum);
+        }
+    }
+}
